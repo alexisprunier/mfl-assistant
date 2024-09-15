b40eb7fee1a13f18fc488d248c8dfde8d97f15d7
Alexis Prunier <alexis.prunier@level.eu>
2024-09-15 15:12:52 +0200

    Update flow signature verification and flow-py-sdk library

--- api/utils/flow.py
+++ api/utils/flow.py
@@ -2,7 +2,7 @@ from flow_py_sdk.exceptions import PySDKError
 from flow_py_sdk.client import AccessAPI, flow_client
 from flow_py_sdk.script import Script
 from flow_py_sdk.utils import CompositeSignature
-from flow_py_sdk import cadence
+from flow_py_sdk import cadence, utils
 from rlp import encode
 
 
@@ -33,55 +33,11 @@ async def verify_signature(service):
             keyId=key_id,
             signature=signature
         )
-        is_verified = await _verify_account_proof_signatures(
-            client=client, message=bytes(fields.hex(), "utf-8"), composite_signatures=[signer]
+
+        signature_is_valid = await utils.verify_user_signature(
+            message=bytes(fields.hex(), "utf-8"),
+            client=client,
+            composite_signatures=[signer],
         )
-        return is_verified
 
-
-async def _verify_account_proof_signatures(*, client: AccessAPI, message: bytes,
-                                          composite_signatures: list[CompositeSignature]
-                                          ) -> bool:
-    # if there is no signature return False
-    if len(composite_signatures) == 0:
-        return False
-
-    # it does not make sense for the signatures to be from different addresses
-    if any(x.addr != composite_signatures[0].addr for x in composite_signatures):
-        raise PySDKError("All signatures must be from the same address")
-
-    address = cadence.Address.from_hex(composite_signatures[0].addr)
-    signatures = cadence.Array(
-        [cadence.String(x.signature) for x in composite_signatures]
-    )
-    key_indexes = cadence.Array([cadence.Int(x.keyId) for x in composite_signatures])
-    cadence_message = cadence.String(str(message, "utf-8"))
-
-    address_of_network = "0xdb6b70764af4ff68" # This is for Mainnet - Testnet address being: 0x5b250a8a85b44a67
-
-    script = Script(
-        code=
-        f'''        
-        import FCLCrypto from {address_of_network}
-        pub fun main(address: Address,
-                     message: String,
-                     keyIndices: [Int],
-                     signatures: [String]): Bool''' + '''{
-            return FCLCrypto.verifyAccountProofSignatures(address: address, message: message, keyIndices: keyIndices, 
-            signatures: signatures)
-         }   
-        ''',
-        arguments=[
-            address,
-            cadence_message,
-            key_indexes,
-            signatures,
-        ],
-    )
-
-    script_result = await client.execute_script(script)
-
-    if script_result is None:
-        return False
-
-    return script_result.as_type(cadence.Bool).value
+        return signature_is_valid

e08e0e9f5a9a16951f176d2db6a2f261bc6b0dc9
Alexis Prunier <alexis.prunier@level.eu>
2025-03-23 11:08:28 +0100

    deactivate match collection

--- api/main.py
+++ api/main.py
@@ -137,7 +137,7 @@ scheduler.add_job(compute_reports.main,             'interval', args=[db, mail],
 scheduler.add_job(collect_clubs.main,               'interval', args=[db],          seconds=60)
 scheduler.add_job(collect_sales.main,               'interval', args=[db],          seconds=30)
 scheduler.add_job(collect_players.main,             'interval', args=[db],          seconds=30)
-scheduler.add_job(collect_matches.main,             'interval', args=[db],          seconds=60 * 3)
+"""scheduler.add_job(collect_matches.main,             'interval', args=[db],          seconds=60 * 3)"""
 scheduler.add_job(compute_raw_player_pricings.main, 'interval', args=[db],          seconds=60 * 5)
 scheduler.add_job(compute_player_pricings.main,     'interval', args=[db],          seconds=60 * 60)
 scheduler.start()

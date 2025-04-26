24e7dfb95648f9159effb6ea58c1d8be910f4321
Alexis Prunier <alexis.prunier@level.eu>
2025-04-26 20:13:11 +0200

    Fix api target

--- web-coach/src/utils/env.js
+++ web-coach/src/utils/env.js
@@ -1,22 +1,20 @@
-
 export const isLocalEnv = () => {
-	return window.location.hostname === "localhost"
-		|| window.location.hostname === "127.0.0.1"
-		|| window.location.hostname === "";
+  return window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1" ||
+    window.location.hostname === "";
 }
 
 export const getMflApiEndpoint = () => {
-	return "https://z519wdyajg.execute-api.us-east-1.amazonaws.com/prod/";
+  return "https://z519wdyajg.execute-api.us-east-1.amazonaws.com/prod/";
 }
 
 export const getApiEndpoint = () => {
-	if (isLocalEnv()) {
-		return "http://127.0.0.1:5000/";
-	}
-	return "https://api." + window.location.hostname.replace("www.", "") + "/";
+  if (isLocalEnv()) {
+    return "http://127.0.0.1:5000/";
+  }
+  return "https://api.mfl-assistant.com/";
 }
 
 export const getGraphQLEndpoint = () => {
-	return getApiEndpoint() + "graphql";
+  return getApiEndpoint() + "graphql";
 }
-
